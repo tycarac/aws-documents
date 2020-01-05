04ada888cc7609cb33c49802606f85cda2be8211
tycarac <>
2020-01-05 22:52:50 +1100

    Tidy up install python packages script

--- install-python-packages.cmd
+++ install-python-packages.cmd
@@ -3,30 +3,49 @@ setlocal
 del /s/q *.bak >nul 2>&1
 
 set logfile=install-python-packages.log
-del /q %logfile%
+del /q %logfile% >nul 2>&1
 
-echo on
-
-if not exist [.\.env] (
+REM ___________________________________________________________________________
+REM Create virtural environment
+if not exist ".\.env" (
+echo Creating virtual environment ...
 python -m venv ".\.env"
 )
-call .env\Scripts\activate.bat
+echo Activating virtual environment ...
+call .env\Scripts\activate
 
-set cmd=python -m pip list --outdated
+REM ___________________________________________________________________________
+REM List Python packaes
+set cmd=python -m pip list --no-color
 echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
 %cmd% | tee.exe -a %logfile%
 
-set cmd=python -m pip install --upgrade pip setuptools wheel virtualenv certifi
+REM ___________________________________________________________________________
+REM List outdated Python packaes
+set cmd=python -m pip list --no-color --outdated 
 echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
 %cmd% | tee.exe -a %logfile%
 
+REM ___________________________________________________________________________
+REM Install/update useful Python packages
+REM Pip "list" output for default Python install is: "pip", "setuptools"
+set cmd=python -m pip install --no-color --compile --upgrade pip setuptools
+echo. | tee.exe -a %logfile%
+echo %cmd% | tee.exe -a %logfile%
+%cmd% | tee.exe -a %logfile%
+
+REM ___________________________________________________________________________
+REM Install/Update packeages from requirements
 if not exist "requirements.txt" goto :eof
 
-set cmd=pip.exe install -U --upgrade-strategy eager -r requirements.txt --compile
-echo cmd %cmd%
+set cmd=pip.exe install --no-color --compile --upgrade-strategy eager --upgrade --requirement requirements.txt
 echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
 %cmd% | tee.exe -a %logfile%
 
+set cmd=python -m pip list --no-color
+echo. | tee.exe -a %logfile%
+echo %cmd% | tee.exe -a %logfile%
+%cmd% | tee.exe -a %logfile%
