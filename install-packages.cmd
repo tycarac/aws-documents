f23eafa1d5fbac6bae29ad4c7f5bb6fdf3015c60
tycarac <>
2020-06-14 20:10:51 +1000

    Added missing tee output to log file

--- install-packages.cmd
+++ install-packages.cmd
@@ -5,15 +5,22 @@ del /s/q *.bak >nul 2>&1
 set logfile=install-python-packages.log
 del /q %logfile% >nul 2>&1
 
+
+REM ___________________________________________________________________________
+REM Output Python version outside virtual environment
+python.exe --version | tee.exe -a %logfile%
+
 REM ___________________________________________________________________________
 REM Create virtural environment
 if not exist ".\.venv" (
 echo Creating virtual environment ...
-python -m venv ".\.venv"
+python -m venv ".\.venv" | tee.exe -a %logfile%
 )
 echo Activating virtual environment ...
-call .venv\Scripts\activate
-python.exe --version
+call .venv\Scripts\activate | tee.exe -a %logfile%
+
+REM Output Python version inside virtual environment
+python.exe --version | tee.exe -a %logfile%
 
 REM ___________________________________________________________________________
 REM List Python packages
@@ -57,7 +64,11 @@ echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
 %cmd% | tee.exe -a %logfile%
 
-REM List Python updated packages
+REM Output Python version inside virtual environment
+echo. | tee.exe -a %logfile%
+python.exe --version | tee.exe -a %logfile%
+
+REM List Python (updated) packages
 set cmd=python -m pip list --no-color
 echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
