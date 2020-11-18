4d3e348a47fa0dffa701a79bf03e64838e41f54f
tycarac <tycarac@gmail.com>
2020-11-18 16:28:56 +1100

    Ensure python executable is called in script

--- install-packages.cmd
+++ install-packages.cmd
@@ -10,7 +10,7 @@ REM ___________________________________________________________________________
 REM Create virtural environment
 if not exist ".\.venv" (
 echo Creating virtual environment ...
-python -m venv ".\.venv"
+python.exe -m venv ".\.venv"
 )
 echo Activating virtual environment ...
 call .venv\Scripts\activate
@@ -20,14 +20,14 @@ python.exe --version
 
 REM ___________________________________________________________________________
 REM List Python packages
-set cmd=python -m pip list --no-color
+set cmd=python.exe -m pip list --no-color
 echo.
 echo %cmd%
 %cmd%
 
 REM ___________________________________________________________________________
 REM List outdated Python packages
-set cmd=python -m pip list --no-color --outdated
+set cmd=python.exe -m pip list --no-color --outdated
 echo.
 echo %cmd%
 %cmd%
@@ -37,11 +37,11 @@ REM Install/update install Python packages
 REM Install/update "pip" first, then "setup", "wheel"
 REM Pip "list" output from default Python 3.8.1 install is: "pip", "setuptools"
 REM https://packaging.python.org/tutorials/installing-packages/#ensure-pip-setuptools-and-wheel-are-up-to-date
-set cmd=python -m pip install --no-color --compile --upgrade pip
+set cmd=python.exe -m pip install --no-color --compile --upgrade pip
 echo.
 echo %cmd%
 %cmd%
-set cmd=python -m pip install --no-color --compile --upgrade setuptools wheel
+set cmd=python.exe -m pip install --no-color --compile --upgrade setuptools wheel
 echo.
 echo %cmd%
 %cmd%
@@ -55,17 +55,17 @@ goto :eof
 )
 
 REM Update using requirements.txt file
-set cmd=python -m pip install --no-color --compile --upgrade-strategy eager --upgrade --requirement requirements.txt
+set cmd=python.exe -m pip install --no-color --compile --upgrade-strategy eager --upgrade --requirement requirements.txt
 echo.
 echo %cmd%
 %cmd%
 
-REM Output Python version inside virtual environment
+REM Output Python version inside virtual environment (again at end)
 echo.
 python.exe --version
 
 REM List Python (updated) packages
-set cmd=python -m pip list --no-color
+set cmd=python.exe -m pip list --no-color
 echo.
 echo %cmd%
 %cmd%
